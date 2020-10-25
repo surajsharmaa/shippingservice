35f4541af5a6cba781a193ff96575349d25da747
sharmas <sharmas@C85HVP2.alsac.local>
2020-10-24 21:05:08 -0500

    Added gmail smtp

--- src/main/java/com/ebook/api/message/ShippingMessage.java
+++ src/main/java/com/ebook/api/message/ShippingMessage.java
@@ -2,41 +2,123 @@ package com.ebook.api.message;
 
 import java.io.Serializable;
 
-public class ShippingMessage implements Serializable{
-	
+public class ShippingMessage implements Serializable {
+
 	private static final long serialVersionUID = -295422703255886286L;
 
+	// shipping info
+	private String recipient_first_name;
 
-  private String to;
-  private String body;
+	private String recipient_last_name;
 
-  public ShippingMessage() {
-  }
+	private String recipient_email;
 
-  public ShippingMessage(String to, String body) {
-    this.to = to;
-    this.body = body;
-  }
+	private String recipient_phone_number;
 
-  public String getTo() {
-    return to;
-  }
+	private String recipient_address1;
 
-  public void setTo(String to) {
-    this.to = to;
-  }
+	private String recipient_city;
 
-  public String getBody() {
-    return body;
-  }
+	private String recipient_state;
 
-  public void setBody(String body) {
-    this.body = body;
-  }
+	private String recipient_zip_code;
+	
+	private String orderId;
+	
+	private String sku;
+	
+	private int quantity;
+	
 
-  @Override
-  public String toString() {
-    return String.format("Email{to=%s, body=%s}", getTo(), getBody());
-  }
+	public String getRecipient_first_name() {
+		return recipient_first_name;
+	}
 
+	public void setRecipient_first_name(String recipient_first_name) {
+		this.recipient_first_name = recipient_first_name;
+	}
+
+	public String getRecipient_last_name() {
+		return recipient_last_name;
+	}
+
+	public void setRecipient_last_name(String recipient_last_name) {
+		this.recipient_last_name = recipient_last_name;
+	}
+
+	public String getRecipient_email() {
+		return recipient_email;
+	}
+
+	public void setRecipient_email(String recipient_email) {
+		this.recipient_email = recipient_email;
+	}
+
+	public String getRecipient_phone_number() {
+		return recipient_phone_number;
+	}
+
+	public void setRecipient_phone_number(String recipient_phone_number) {
+		this.recipient_phone_number = recipient_phone_number;
+	}
+
+	public String getRecipient_address1() {
+		return recipient_address1;
+	}
+
+	public void setRecipient_address1(String recipient_address1) {
+		this.recipient_address1 = recipient_address1;
+	}
+
+	public String getRecipient_city() {
+		return recipient_city;
+	}
+
+	public void setRecipient_city(String recipient_city) {
+		this.recipient_city = recipient_city;
+	}
+
+	public String getRecipient_state() {
+		return recipient_state;
+	}
+
+	public void setRecipient_state(String recipient_state) {
+		this.recipient_state = recipient_state;
+	}
+
+	public String getRecipient_zip_code() {
+		return recipient_zip_code;
+	}
+
+	public void setRecipient_zip_code(String recipient_zip_code) {
+		this.recipient_zip_code = recipient_zip_code;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
 }
--- src/main/java/com/ebook/shippingservice/messagelistener/ShippingMessageHandler.java
+++ src/main/java/com/ebook/shippingservice/messagelistener/ShippingMessageHandler.java
@@ -1,17 +1,47 @@
 package com.ebook.shippingservice.messagelistener;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 import com.ebook.api.message.ShippingMessage;
 
 @Component
 public class ShippingMessageHandler {
+	@Autowired
+	private JavaMailSender javaMailSender;
 
-  @JmsListener(destination = "shipping")
-  public void receiveMessage(ShippingMessage email) {
-    System.out.println("Received <" + email.getBody() + ">");
-  }
+	@JmsListener(destination = "shipping")
+	public void receiveMessage(ShippingMessage shippingMsg) {
+		System.out.println("Sending shipping request to vendor for orderId: <" + shippingMsg.getOrderId() + ">");
+		try {
+			sendEmail(shippingMsg);
+		} catch (Exception e) {
+			System.out.println("Error with sending email.");
+		}
+	}
 
+	public void sendEmail(ShippingMessage shippingMsg) {
+
+		SimpleMailMessage msg = new SimpleMailMessage();
+		msg.setTo("surajsapkota1@gmail.com", "srosesuraj@yahoo.com");
+
+		msg.setSubject("Ship Order Request!");
+		msg.setText("Please ship following order in the given shipping address.\n\n" + "Order information:\n"
+				+ "Order Id:" + shippingMsg.getOrderId() + "\n" + "SKU:" + shippingMsg.getSku() + "\n" + "Quantity:"
+				+ shippingMsg.getQuantity() + "\n\n"
+
+				+ "Shipping Details:\n" + shippingMsg.getRecipient_first_name() + " " + shippingMsg.getRecipient_last_name() + "\n"
+				+ shippingMsg.getRecipient_address1() + "\n" + shippingMsg.getRecipient_city() + ", " + shippingMsg.getRecipient_state()
+				+ ", " + shippingMsg.getRecipient_zip_code() + "\n" + "Phone: " + shippingMsg.getRecipient_phone_number() + "\n" + "Email: " + shippingMsg.getRecipient_email() + "\n\n"
+
+				+ "Contact us if you have any questions about your order.\n\n Ebook"
+
+		);
+
+		javaMailSender.send(msg);
+
+	}
 }
